f697c74abcea1520b62bfca83cfc965fb71dbf38
timoxoszt <20520810@gm.uit.edu.vn>
2023-02-12 19:30:19 +0700

    laravel/breeze

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -17,3 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
